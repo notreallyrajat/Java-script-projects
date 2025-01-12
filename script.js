62c3500e68f516c1f47fad6be5bec084067b8730
Rajat <158155868+notreallyrajat@users.noreply.github.com>
2025-01-12 13:18:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const bodyEl = document.querySelector("body")
+
+bodyEl.addEventListener("mousemove",() => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left  = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random()*200;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px"
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove()
+    },800 )
+    
+})
+
